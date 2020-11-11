fb7270cf3d9ee9e3271fb51cdf61525c88d074f6
Paul Stoute <paul.stoute@gmail.com>
2020-11-11 12:53:07 -0800

    added form action and method

--- resources/views/team/create.blade.php
+++ resources/views/team/create.blade.php
@@ -6,7 +6,8 @@
             <div class="card">
                 <h3 class="card-header">Create a team</h3>
                 <div class="card-body">
-                    <form class="form">
+                    <form class="form" method="POST" action="{{url('/teams/create')}}">
+                        @csrf
                         <input class="form-control" type="text" name="name" placeholder="Team Name">
                     </form>
                 </div>
