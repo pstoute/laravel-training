6fc11a5ff3aad3f58960127e5b1ff5794196ae36
Paul Stoute <paul.stoute@gmail.com>
2020-11-11 11:38:07 -0800

    Guarded and Added string to migrations

--- app/Models/Tickets.php
+++ app/Models/Tickets.php
@@ -8,4 +8,6 @@ use Illuminate\Database\Eloquent\Model;
 class Tickets extends Model
 {
     use HasFactory;
+
+    $guarded = [];
 }
--- database/migrations/2020_11_11_192203_create_teams_table.php
+++ database/migrations/2020_11_11_192203_create_teams_table.php
@@ -15,6 +15,7 @@ class CreateTeamsTable extends Migration
     {
         Schema::create('teams', function (Blueprint $table) {
             $table->id();
+            $table->string('name')->notnull();
             $table->timestamps();
         });
     }
